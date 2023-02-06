be8bbe2da12f77b849832281a74409a4de30a199
Sanjeev718191 <sanjeev718191@gmail.com>
2023-02-06 21:24:24 +0530

    Video call implemented

--- app/src/main/java/com/sk/videocall_agora/MainActivity.java
+++ app/src/main/java/com/sk/videocall_agora/MainActivity.java
@@ -1,14 +1,170 @@
 package com.sk.videocall_agora;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import com.sk.videocall_agora.databinding.ActivityMainBinding;
+
+import io.agora.rtc2.ChannelMediaOptions;
+import io.agora.rtc2.Constants;
+import io.agora.rtc2.IRtcEngineEventHandler;
+import io.agora.rtc2.RtcEngine;
+import io.agora.rtc2.RtcEngineConfig;
+import io.agora.rtc2.video.VideoCanvas;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String appId = "5d0523b08aa340b1abf2d88af087944f";
+    private String channelName = "SK Stream";
+    private String token = "007eJxTYMjfty+mxjM8dXYE/+PG5+vi1m2b7nNdeJPaurutHy0dpqcrMJimGJgaGScZWCQmGpsYJBkmJqUZpVhYJKYZWJhbmpikXb36ILkhkJGh2MOIkZEBAkF8ToZgb4XgkqLUxFwGBgBVmyJO";
+    private int uid = 0;
+    private boolean isJoined = false;
+    private RtcEngine agoraEngine;
+    private SurfaceView localSurfaceView;
+    private SurfaceView remoteSurfaceView;
+
+    ActivityMainBinding binding;
+    private int PERMISSION_id = 12;
+    String permissions[] = new String[]{Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO};
+
+    private boolean checkSelfPermission() {
+        if (ContextCompat.checkSelfPermission(this, permissions[0]) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, permissions[1]) != PackageManager.PERMISSION_GRANTED)
+            return false;
+        return true;
+    }
+
+    void showMessage(String message) {
+        runOnUiThread(() ->
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show());
+    }
+
+    private void setupVideoSDKEngine() {
+        try {
+            RtcEngineConfig config = new RtcEngineConfig();
+            config.mContext = getBaseContext();
+            config.mAppId = appId;
+            config.mEventHandler = mRtcEventHandler;
+            agoraEngine = RtcEngine.create(config);
+            // By default, the video module is disabled, call enableVideo to enable it.
+            agoraEngine.enableVideo();
+        } catch (Exception e) {
+            showMessage(e.toString());
+        }
+    }
+
+    private final IRtcEngineEventHandler mRtcEventHandler = new IRtcEngineEventHandler() {
+        @Override
+        // Listen for the remote host joining the channel to get the uid of the host.
+        public void onUserJoined(int uid, int elapsed) {
+            showMessage("Remote user joined " + uid);
+            runOnUiThread(() -> setupRemoteVideo(uid));
+        }
+
+        @Override
+        public void onJoinChannelSuccess(String channel, int uid, int elapsed) {
+            isJoined = true;
+            showMessage("Joined Channel " + channel);
+        }
+
+        @Override
+        public void onUserOffline(int uid, int reason) {
+            showMessage("Remote user offline " + uid + " " + reason);
+            runOnUiThread(() -> remoteSurfaceView.setVisibility(View.GONE));
+        }
+    };
+
+    private void setupRemoteVideo(int uid) {
+        FrameLayout container = findViewById(R.id.remote_user);
+        remoteSurfaceView = new SurfaceView(getBaseContext());
+        remoteSurfaceView.setZOrderMediaOverlay(true);
+        container.addView(remoteSurfaceView);
+        agoraEngine.setupRemoteVideo(new VideoCanvas(remoteSurfaceView, VideoCanvas.RENDER_MODE_FIT, uid));
+        // Display RemoteSurfaceView.
+        remoteSurfaceView.setVisibility(View.VISIBLE);
+    }
+
+    private void setupLocalVideo() {
+        FrameLayout container = findViewById(R.id.local_user);
+        // Create a SurfaceView object and add it as a child to the FrameLayout.
+        localSurfaceView = new SurfaceView(getBaseContext());
+        container.addView(localSurfaceView);
+        // Pass the SurfaceView object to Agora so that it renders the local video.
+        agoraEngine.setupLocalVideo(new VideoCanvas(localSurfaceView, VideoCanvas.RENDER_MODE_HIDDEN, 0));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        if (!checkSelfPermission()) {
+            ActivityCompat.requestPermissions(this, permissions, PERMISSION_id);
+        }
+
+        setupVideoSDKEngine();
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        agoraEngine.stopPreview();
+        agoraEngine.leaveChannel();
+
+        // Destroy the engine in a sub-thread to avoid congestion
+        new Thread(() -> {
+            RtcEngine.destroy();
+            agoraEngine = null;
+        }).start();
+    }
+
+    private void leaveCall() {
+    }
+
+    public void joinChannel(View view) {
+        if (checkSelfPermission()) {
+            ChannelMediaOptions options = new ChannelMediaOptions();
+
+            // For a Video call, set the channel profile as COMMUNICATION.
+            options.channelProfile = Constants.CHANNEL_PROFILE_COMMUNICATION;
+            // Set the client role as BROADCASTER or AUDIENCE according to the scenario.
+            options.clientRoleType = Constants.CLIENT_ROLE_BROADCASTER;
+            // Display LocalSurfaceView.
+            setupLocalVideo();
+            localSurfaceView.setVisibility(View.VISIBLE);
+            // Start local preview.
+            agoraEngine.startPreview();
+            // Join the channel with a temp token.
+            // You need to specify the user ID yourself, and ensure that it is unique in the channel.
+            agoraEngine.joinChannel(token, channelName, uid, options);
+        } else {
+            Toast.makeText(getApplicationContext(), "Permissions was not granted", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void leaveChannel(View view) {
+        if (!isJoined) {
+            showMessage("Join a channel first");
+        } else {
+            agoraEngine.leaveChannel();
+            showMessage("You left the channel");
+            // Stop remote video rendering.
+            if (remoteSurfaceView != null) remoteSurfaceView.setVisibility(View.GONE);
+            // Stop local video rendering.
+            if (localSurfaceView != null) localSurfaceView.setVisibility(View.GONE);
+            isJoined = false;
+        }
     }
 }
